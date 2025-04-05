56cfa89e7b6385cf5160e337914a38525aca3515
Carlos Martinez <camartinez7@hotmail.com>
2025-04-04 19:05:10 -0500

    add exersice twoSum

--- ArraysStringsHashTables/src/test/java/org/example/TwoSumTest.java
+++ ArraysStringsHashTables/src/test/java/org/example/TwoSumTest.java
@@ -8,11 +8,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class TwoSumTest {
 
     @Test
-    @DisplayName("is Unique Test Method 9256")
+    @DisplayName("TwoSum Test Method 9256")
     void twoSumTest9256() {
         int[] result = TwoSum.twoSum(new int[] {9, 2, 5, 6}, 7);
         // Arrays.stream(result).forEach(System.out::println);
         assertTrue((result[0] == 1 && result[1] == 2) || (result[0] == 2 && result[1] == 1));
+    }
+
+    @Test
+    @DisplayName("TwoSum Test Method 271115")
+    void twoSumTest271115() {
+        int[] result = TwoSum.twoSum(new int[] {2, 7, 11, 15}, 9);
+        assertTrue((result[0] == 0 && result[1] == 1) || (result[0] == 1 && result[1] == 0));
+    }
+
+    @Test
+    @DisplayName("TwoSum Test Method -34390")
+    void twoSumTestNeg34390() {
+        int[] result = TwoSum.twoSum(new int[] {-3, 4, 3, 90}, 0);
+        assertTrue((result[0] == 0 && result[1] == 2) || (result[0] == 2 && result[1] == 0));
     }
 
 }
